215c54636b689c8e9ad59ea0a19cd160e8489298
Zachary Zhu <zacharyzhu@berkeley.edu>
2020-01-22 17:19:33 -0800

    Completed Year.java

--- lab1/Year.java
+++ lab1/Year.java
@@ -5,7 +5,10 @@ public class Year {
 
     /** Return true iff YEAR is a leap year.  */
     static boolean isLeapYear(int year) {
-        return true;    // TODO: YOUR CODE HERE
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
+          return true;
+        }
+        return false;
     }
 
     /** Print whether YEAR is a a leap year on System.out. */
@@ -35,4 +38,3 @@ public class Year {
         }
     }
 }
-
