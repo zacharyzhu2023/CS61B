4f0438ae3e403f845e6ddb5487a04fbd70157c4f
Zachary Zhu <zacharyzhu@berkeley.edu>
2020-01-24 21:21:34 -0800

    Added the tests to Tester.java

--- hw0/Tester.java
+++ hw0/Tester.java
@@ -3,8 +3,8 @@ import static org.junit.Assert.*;
 
 import ucb.junit.textui;
 
-/** Tests for hw0. 
- *  @author YOUR NAMES HERE
+/** Tests for hw0.
+ *  @author Zachary Zhu
  */
 public class Tester {
 
@@ -13,7 +13,7 @@ public class Tester {
      * testing later.
      *
      * To actually run the tests, just use
-     *      java Tester 
+     *      java Tester
      * (after first compiling your files).
      *
      * DON'T put your HW0 solutions here!  Put them in a separate
@@ -25,21 +25,29 @@ public class Tester {
     public void maxTest() {
         // Change call to max to make this call yours.
         assertEquals(14, max(new int[] { 0, -5, 2, 14, 10 }));
-        // REPLACE THIS WITH MORE TESTS.
+        assertEquals(0, max(new int[] {0}));
+        assertEquals(12, max(new int[] {2, 5, 10, -11, 12}));
+
     }
 
     @Test
     public void threeSumTest() {
         // Change call to threeSum to make this call yours.
         assertTrue(threeSum(new int[] { -6, 3, 10, 200 }));
-        // REPLACE THIS WITH MORE TESTS.
+        assertTrue(threeSum(new int[] { -10, 10, 5 }));
+        assertFalse(threeSum(new int[] { -5, -6, -7, -8 }));
+        assertTrue(threeSum(new int[] { 0, 6, 1 }));
+
     }
 
     @Test
     public void threeSumDistinctTest() {
         // Change call to threeSumDistinct to make this call yours.
         assertFalse(threeSumDistinct(new int[] { -6, 3, 10, 200 }));
-        // REPLACE THIS WITH MORE TESTS.
+        assertTrue(threeSumDistinct(new int[] { -5, 5, 0}));
+        assertFalse(threeSumDistinct(new int[] { 0, 6, 1, -5 }));
+        assertTrue(threeSumDistinct(new int[] { -10, -5, 14, 13, 100, 15 }));
+
     }
 
     public static void main(String[] unused) {
