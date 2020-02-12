260bb01398001a7583aad0ca698a25d677c8572e
Zachary Zhu <zacharyzhu@berkeley.edu>
2020-02-11 23:27:12 -0800

    style change 13

--- proj0/signpost/Model.java
+++ proj0/signpost/Model.java
@@ -676,8 +676,7 @@ class Model implements Iterable<Model.Sq> {
                     }
                     if (this.predecessor() != null) {
                         this._head._group = newGroup();
-                    } else {
-                        this._group = -1;
+                    } else { this._group = -1;
                     }
                 }
                 boolean nNum = false; Sq nextPointer0 = next;
