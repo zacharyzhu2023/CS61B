11643599c57760fd6e4792622d45b84e38222929
Zachary Zhu <zacharyzhu@berkeley.edu>
2020-02-26 22:49:36 -0800

    t

--- lab6/enigma/PermutationTest.java
+++ lab6/enigma/PermutationTest.java
@@ -193,10 +193,10 @@ public abstract class PermutationTest {
     @Test
     public void checkAlphabetTest() {
         Permutation p1 = getNewPermutation("(BACD)", getNewAlphabet("ABCD"));
-        //assertTrue(p1.alphabet().equals("ABCD"));
+        assertTrue(p1.alphabet().equals("ABCD"));
         Permutation p2 = getNewPermutation("(AB) (CEF)", getNewAlphabet("ABCEFGW"));
         //assertTrue(p2.alphabet().equals("ABCEFGW"));
         Permutation p3 = getNewPermutation("(A) (CWE)", getNewAlphabet("ABCEFGW"));
-        assertTrue(p3.alphabet().equals("ABCEFGW"));
+        //assertTrue(p3.alphabet().equals("ABCEFGW"));
     }
 }
