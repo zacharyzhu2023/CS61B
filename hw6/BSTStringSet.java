41b1762fb9eafcd09cc6319c27d582115fcc57da
Zachary Zhu <zacharyzhu@berkeley.edu>
2020-03-17 12:26:05 -0700

    Working on BSTSortedStringSet

--- hw6/BSTStringSet.java
+++ hw6/BSTStringSet.java
@@ -6,9 +6,14 @@ import java.util.Stack;
 
 /**
  * Implementation of a BST based String Set.
- * @author
+ * @author Zachary Zhu
  */
-public class BSTStringSet implements StringSet, Iterable<String> {
+
+public class BSTStringSet implements SortedStringSet, Iterable<String> {
+
+    /** Root node of the tree. */
+    private Node _root;
+
     /** Creates a new empty set. */
     public BSTStringSet() {
         _root = null;
@@ -16,19 +21,80 @@ public class BSTStringSet implements StringSet, Iterable<String> {
 
     @Override
     public void put(String s) {
-        // FIXME: PART A
+        findAndPutParent(s, _root);
     }
 
+    /** Helper function for put using root Node */
+    public Node findAndPutParent(String str, Node n) {
+        if (n == null) {
+            _root = new Node(str);
+            return null;
+        } else if (str.compareTo(n.s) == 0) {
+            return n;
+        } else if (str.compareTo(n.s) > 0 && n.right == null) {
+            n.right = new Node(str);
+            return n;
+        } else if (str.compareTo(n.s) < 0 && n.left == null) {
+            n.left = new Node(str);
+            return n;
+        } else if (str.compareTo(n.s) > 0) {
+            return findAndPutParent(str, n.right);
+        } else {
+            return findAndPutParent(str, n.left);
+        }
+    }
+
+    /** Helper function for contains */
+    public boolean containsHelper(String s, Node n) {
+        if (n == null) {
+            return false;
+        } else if (n.getLabel().equals(s)) {
+            return true;
+        } else if (n.getLabel().compareTo(s) > 0) {
+            return containsHelper(s, n.left);
+        } else {
+            return containsHelper(s, n.right);
+        }
+    }
     @Override
     public boolean contains(String s) {
-        return false; // FIXME: PART A
+        return containsHelper(s, _root);
     }
 
     @Override
     public List<String> asList() {
-        return null; // FIXME: PART A
+        List<String> listStrings = new ArrayList<String>();
+        BSTIterator iter = new BSTIterator(_root);
+        while (iter.hasNext()) {
+            String nextElem = iter.next();
+            listStrings.add(nextElem);
+            //System.out.println(nextElem);
+        }
+        return listStrings;
     }
 
+    // Tester for range iterator
+    public void printRange() {
+        BSTStringSetinOrderIterator rangeIter = new BSTStringSetinOrderIterator(_root, "ia", "zz");
+        while (rangeIter.hasNext()) {
+            String nextElem = rangeIter.next();
+            System.out.println(nextElem);
+        }
+    }
+
+    public String getBSTLabel() {
+        return _root.getLabel();
+    }
+
+    /** Returns the left node. */
+    public Node getLeftNode() {
+        return _root.left;
+    }
+
+    /** Returns the right node. */
+    public Node getRightNode() {
+        return _root.right;
+    }
 
     /** Represents a single Node of the tree. */
     private static class Node {
@@ -43,9 +109,25 @@ public class BSTStringSet implements StringSet, Iterable<String> {
         Node(String sp) {
             s = sp;
         }
+        /** Return the left node. */
+        Node getLeft() {
+            return left;
+        }
+        /** Return the right node. */
+        Node getRight() {
+            return right;
+        }
+        /** Get the label of the current node. */
+        String getLabel() {
+            return s;
+        }
     }
 
+
+
+
     /** An iterator over BSTs. */
+    // How can we restrict the range of values that it can iterate over?
     private static class BSTIterator implements Iterator<String> {
         /** Stack of nodes to be delivered.  The values to be delivered
          *  are (a) the label of the top of the stack, then (b)
@@ -96,12 +178,72 @@ public class BSTStringSet implements StringSet, Iterable<String> {
     }
 
     // FIXME: UNCOMMENT THE NEXT LINE FOR PART B
-    // @Override
+    // @Override was previously commented out
+    @Override
     public Iterator<String> iterator(String low, String high) {
-        return null;  // FIXME: PART B
+        return new BSTStringSetinOrderIterator(_root, low, high);
     }
 
+    /** SECONDARY CLASS */
+    private static class BSTStringSetinOrderIterator implements Iterator<String>  {
+        /** Stack of nodes to be delivered.  The values to be delivered
+         *  are (a) the label of the top of the stack, then (b)
+         *  the labels of the right child of the top of the stack inorder,
+         *  then (c) the nodes in the rest of the stack (i.e., the result
+         *  of recursively applying this rule to the result of popping
+         *  the stack. */
+        private Stack<Node> _toDo = new Stack<>();
 
-    /** Root node of the tree. */
-    private Node _root;
+        /** A new iterator over the labels in NODE. */
+        BSTStringSetinOrderIterator(Node node, String low, String high) {
+            _low = low;
+            _high = high;
+            addTree(node);
+
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !_toDo.empty();
+        }
+
+        @Override
+        public String next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            Node node = _toDo.pop();
+            addTree(node.right);
+            return node.s;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        /** Add the relevant subtrees of the tree rooted at NODE. */
+        private void addTree(Node node) {
+            if (node != null) {
+                if (node.s.compareTo(_high) >= 0) { // Case of too high a value
+                    addTree(node.left);
+                }
+
+                else if (node.s.compareTo(_low) < 0) { // Case of too low a value
+                    addTree(node.right);
+                }
+
+                else { // We are within the range
+                    _toDo.push(node);
+                    addTree(node.left);
+                }
+            }
+        }
+
+        private String _low;
+        private String _high;
+
+    }
 }
+
--- hw6/BSTStringSetTest.java
+++ hw6/BSTStringSetTest.java
@@ -1,16 +1,70 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Test of a BST-based String Set.
- * @author
+ * @author Zachary Zhu
  */
 public class BSTStringSetTest  {
     // FIXME: Add your own tests for your BST StringSet
 
     @Test
-    public void testNothing() {
-        // FIXME: Delete this function and add your own tests
+    public void testPut() {
+        BSTStringSet t = new BSTStringSet();
+        t.put("hello");
+        t.put("good");
+        t.put("salutations");
+        t.put("zz");
+        t.put("igloo");
+        t.put("iii");
+        t.put("iiii");
+        assertTrue(t.getBSTLabel().equals("hello"));
     }
+
+    @Test
+    public void testContains() {
+        BSTStringSet t = new BSTStringSet();
+        t.put("hello");
+        t.put("good");
+        t.put("salutations");
+        t.put("zz");
+        t.put("igloo");
+        t.put("iii");
+        t.put("iiii");
+        assertTrue(t.contains("hello"));
+        assertTrue(t.contains("zz"));
+        assertFalse(t.contains("iiiii"));
+        assertFalse(t.contains("tester"));
+
+    }
+
+    @Test
+    public void testasList() {
+        BSTStringSet t = new BSTStringSet();
+        t.put("hello");
+        t.put("good");
+        t.put("salutations");
+        t.put("zz");
+        t.put("igloo");
+        t.put("iii");
+        t.put("iiii");
+        t.asList();
+    }
+
+    @Test
+    public void testRangeIterator() {
+        BSTStringSet t = new BSTStringSet();
+        t.put("hello");
+        t.put("good");
+        t.put("salutations");
+        t.put("zz");
+        t.put("igloo");
+        t.put("iii");
+        t.put("iiii");
+        t.printRange();
+    }
+
 }
