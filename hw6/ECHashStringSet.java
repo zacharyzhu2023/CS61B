2a723a32b8a629c2eb0aae8604f54faee88ac114
Zachary Zhu <zacharyzhu@berkeley.edu>
2020-03-22 12:26:53 -0700

    Finished hw6 hopefully

--- hw6/ECHashStringSet.java
+++ hw6/ECHashStringSet.java
@@ -1,21 +1,70 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /** A set of String values.
  *  @author Zachary Zhu
  */
 class ECHashStringSet implements StringSet {
+    // Load Factor = # items/ # buckets
+    private double loadFactorMin = 0.2;
+    private double loadFactorMax = 5;
+    private LinkedList<String>[] buckets;
+    private int numItems;
+
+    public ECHashStringSet () {
+        buckets = new LinkedList[5];
+        for (int i = 0; i < 5; i += 1) {
+            buckets[i] = new LinkedList<String>();
+        }
+        numItems = 0;
+    }
+
+
+    private int hashCodeConverter(String s) {
+        if (s.hashCode() < 0)
+            return (s.hashCode() & 0x7fffffff) % buckets.length;
+        else
+            return s.hashCode();
+    }
+
+    private void resize() {
+        LinkedList<String> [] new_buckets = new LinkedList[buckets.length * 5];
+        for (int i = 0; i < new_buckets.length; i += 1) {
+            new_buckets[i] = new LinkedList<String>();
+        }
+        for (int i = 0; i < buckets.length; i += 1) {
+            for (String s : buckets[i]) {
+                int hashCode = hashCodeConverter(s);
+                new_buckets[hashCode % new_buckets.length].add(s);
+            }
+        }
+        buckets = new_buckets;
+    }
+
     @Override
     public void put(String s) {
-        // FIXME
+        numItems += 1;
+        if (numItems / buckets.length > loadFactorMax) {
+            resize();
+        }
+        int hashCode = hashCodeConverter(s);
+        buckets[hashCode % buckets.length].add(s);
     }
 
     @Override
     public boolean contains(String s) {
-        return false; // FIXME
+        return buckets[hashCodeConverter(s) % buckets.length].contains(s);
     }
 
     @Override
     public List<String> asList() {
-        return null; // FIXME
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < buckets.length; i += 1) {
+            for (String s : buckets[i]) {
+                result.add(s);
+            }
+        }
+        return result;
     }
 }
--- hw6/ECHashStringSetTest.java
+++ hw6/ECHashStringSetTest.java
@@ -10,7 +10,17 @@ public class ECHashStringSetTest  {
     // FIXME: Add your own tests for your ECHashStringSetTest
 
     @Test
-    public void testNothing() {
-        // FIXME: Delete this function and add your own tests
+    public void testECStringSet() {
+        ECHashStringSet tester = new ECHashStringSet();
+        tester.put("one");
+        tester.put("two");
+        tester.put("three");
+        tester.put("four");
+        tester.put("five");
+        tester.put("again");
+        tester.put("another");
+        System.out.println(tester.asList());
+        assertTrue(tester.contains("another"));
+        assertFalse(tester.contains("once"));
     }
 }
